9c459f723d0761658747854fd3f226ff0906f67c
Md. Miraz Faruk <mfaruk172113@bscse.uiu.ac.bd>
2021-03-23 23:55:21 +0600

    update path.php file

--- path.php
+++ path.php
@@ -1,6 +1,6 @@
 <?php
 
-$local_path = 'http://localhost:8080/reviewtest';
+$local_path = 'http://localhost/reviewnow';
 $login_path = $local_path.'/login/login.php';
 $reg_path = $local_path.'/reg_form.php';
 
