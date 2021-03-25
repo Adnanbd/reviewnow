481ea3ce8fdbbb0cc7c783a70aee57366c5ccb4d
Arafath-Rahman <mkhan172093@bscse.uiu.ac.bd>
2021-03-25 21:10:50 +0600

    added basic html structure and navbar for the profile page

--- homepage/profile.php
+++ homepage/profile.php
@@ -12,3 +12,106 @@ $row_one = mysqli_fetch_array($user_email);
  
 
 ?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	
+	<!--<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">-->
+	<!--<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>-->
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Review IT</title>
+    
+    <style>
+	
+	
+    </style>
+    
+  </head>
+  
+  
+  <body>
+  
+   <!-- navbar starts -->
+   
+   
+           <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light navbar-dark bg-dark ">
+		   
+		   
+              
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+			  
+	
+                          
+                       
+             
+			  
+				
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+			  
+			  
+               
+                <ul class="navbar-nav mx-auto">
+				
+				
+                 
+                  <li class="nav-item active">
+                    <a class="nav-link" href="<?php echo $home_path; ?>">HOME</a>
+                  </li>
+                  
+                  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $create_post_path; ?>">Post a Review</a>
+                  </li>
+                  
+				  
+				  
+				 
+                </ul>
+				 <ul class="navbar-nav ml-auto">
+            <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $profile_path; ?>"><?php echo "<strong>Welcome, ".$row_one["first_name"]." ".$row_one["last_name"]."</strong>"; ?></a>
+                  </li>
+                  
+   
+				  
+				  <li class="nav-item">
+                    <a class="nav-link" href="<?php echo $profile_path; ?>" >Log Out</a>
+                  </li>
+        </ul>
+				
+				
+	
+              </div>
+            </nav>
+
+   <!-- navbar ends -->
+  
+<!-- design -->
+
+	 
+<!-- design -->				
+
+
+
+      
+    
+
+    
+    
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="scripts.js"></script>
+  </body>
+</html>
