fc98a44682ffacb1bed7d7d9f88ff0a15cceef39
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-03-26 22:00:15 +0600

    Write codes for user logout in logout.php file

---
+++ homepage/logout.php
@@ -0,0 +1,24 @@
+<?php
+ session_start(); 
+ session_destroy();
+ include 'C:\xampp\htdocs\reviewbow\path.php';
+
+
+
+
+										 echo"
+								   
+								   <script>
+								   alert ('Logout, Log in Please');
+								  
+								   window.location.href='$login_path';
+								   </script>
+								   
+								   ";
+
+
+
+
+
+
+?>
