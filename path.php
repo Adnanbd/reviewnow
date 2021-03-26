0052588784eb1163cd8b9530320a66ca50a3b346
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-03-26 22:04:28 +0600

    update path.php file

--- path.php
+++ path.php
@@ -1,8 +1,14 @@
 <?php
 
-$local_path = 'http://localhost/reviewnow';
+$local_path = 'http://localhost:8080/reviewnow';
 $login_path = $local_path.'/login/login.php';
 $reg_path = $local_path.'/reg_form.php';
+$home_path = $local_path.'/homepage/Homepage.php';
+$create_post_path = $local_path.'/homepage/post_a_review.php';
+$profile_path = $local_path.'/homepage/profile.php';
+$logout_path = $local_path.'/homepage/logout.php';
+
+
 
 
 
