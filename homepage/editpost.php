d6379020150e7bc2303a3e7b22c82b4051ff0501
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-03-26 18:46:29 +0600

    include db connection file and path file and load database

--- homepage/editpost.php
+++ homepage/editpost.php
@@ -1,4 +1,11 @@
 <?php
+session_start();
+include 'C:\xampp\htdocs\reviewtest\db_connect.php';
+include 'C:\xampp\htdocs\reviewtest\path.php';
+$tempid = $_GET["id"];
+$user_email = mysqli_query($con, "SELECT * FROM users where email_id = '" . $_SESSION["email"] . "' ");
+
+$row_one = mysqli_fetch_array($user_email);
 
 
 
