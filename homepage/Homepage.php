f31d79b32a26f7ae87f28542a530470928b1fbd3
Nadia akther <nakther172114@bscse.uiu.ac.bd>
2021-03-26 21:38:47 +0600

    Load all reviews from database

--- homepage/Homepage.php
+++ homepage/Homepage.php
@@ -1,5 +1,13 @@
 <?php
+session_start(); 
+include 'C:\xampp\htdocs\reviewtest\db_connect.php';
+include 'C:\xampp\htdocs\reviewtest\path.php';
 
+ $result = mysqli_query($con, "SELECT * FROM review ORDER BY review_id DESC");
+ 
+ $user_email = mysqli_query($con, "SELECT * FROM users where email_id = '".$_SESSION["email"]."' ");
+ 
+ $row_one = mysqli_fetch_array($user_email);
  
 
 ?>
@@ -20,9 +28,6 @@
     
     <style>
 	
-	
-   
-  
    
 
   
