22531facb88d08846b884d6a446c6ab66efbac5b
Nadia akther <nakther172114@bscse.uiu.ac.bd>
2021-05-27 01:14:01 +0600

    setup basic html code for UI

--- homepage/filter_review.php
+++ homepage/filter_review.php
@@ -15,3 +15,16 @@ $user_email = mysqli_query($con, "SELECT * FROM users where email_id = '".$_SESS
 		
 
 ?>
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Review IT</title>
+    
+    <style>
