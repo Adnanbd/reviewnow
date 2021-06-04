c140740f20b8d995d9ea0a31b3e579be9308f350
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-06-04 17:59:04 +0600

    Update the UI part

--- homepage/profile.php
+++ homepage/profile.php
@@ -249,10 +249,10 @@ $row_one = mysqli_fetch_array($user_email);
   echo "<img src='images/user.jpg'>";
   echo "<div id='proinfo'>";
   ?>
-  echo "<h2><strong>".$row_one["first_name"]." ".$row_one["last_name"]."</strong></h2>";
-  echo "<p>"."<b>Address: </b>".$row_one["address"]."</p>";
-  echo "<p>"."<b>Date-of-Birth: </b>".$row_one["date_of_birth"]."</p>";
-  echo "<p>"."<b>Phone: </b>".$row_one["phone_no"]."</p>";
+  <h2 id = "fullName"><strong></strong></h2>
+  <p id = "address"><b></b></p>
+  <p id = "dob"><b></b></p>
+  <p id = "phoneno"><b></b></p>
   <?php
   echo "</div>";
   echo "</div>";
