647b29bba45faa5a08912efe1bd08a06d0455a95
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-06-04 18:02:23 +0600

    Create profile_backend.php and write PHP codes to fetch data from database

---
+++ homepage/profile_backend.php
@@ -0,0 +1,31 @@
+<?php
+
+$email = $_GET['email'];
+include 'C:\xampp\htdocs\reviewtest\db_connect.php';
+
+error_reporting(0);
+
+
+$status = 'OK';
+$content = [];
+
+if (mysqli_connect_errno()) {
+	$status = 'ERROR';
+	$content = mysqli_connect_error();
+}
+
+$query = "SELECT * FROM users where email_id = '$email'";
+
+
+if ($result = mysqli_query($con, $query)) {
+    /* fetch associative array */
+    while ($row = mysqli_fetch_assoc($result)) {
+        $content[] = $row;	// push value to array
+    }
+}
+
+$data = ["status" => $status, "content" => $content];
+
+header('Content-type: application/json');
+echo json_encode($data); // get all products in json format.    
+?>
