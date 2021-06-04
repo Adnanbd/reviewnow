4b06384ce1ea4e13ab94c51d602dbc801930df30
Md. Shoeb Adnan (011 172 095) <madnan172095@bscse.uiu.ac.bd>
2021-06-04 18:00:29 +0600

    Write Script for showing fetched user data

--- homepage/profile.php
+++ homepage/profile.php
@@ -369,21 +369,28 @@ $row_one = mysqli_fetch_array($user_email);
   ?>
   	</div>
 	 
-<!-- design -->				
+<script type="text/javascript">
+		
+			var xmlhttp = new XMLHttpRequest();
 
-
-
-      
-    
-
-    
-    
-
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
-    <script src="scripts.js"></script>
+			xmlhttp.onreadystatechange = function() {
+			  if (this.readyState == 4 && this.status == 200) {
+			  
+			    var myObj = JSON.parse(this.responseText);
+				
+				document.getElementById("fullName").innerHTML = "Name : " + myObj.content[0]['first_name'] +" "+ myObj.content[0]['last_name'];
+				document.getElementById("address").innerHTML = "Address : " + myObj.content[0]['address'];
+				document.getElementById("dob").innerHTML = "Date of Birth : " + myObj.content[0]['date_of_birth'];
+				document.getElementById("phoneno").innerHTML = "Phone : " + myObj.content[0]['phone_no'];
+			
+			   
+			  }
+			};
+			var mail = '<?php echo $_SESSION["email"];?>';
+			xmlhttp.open("GET", "profile_backend.php?email="+ mail, true);
+			xmlhttp.send();
+		
+		
+	</script>
   </body>
 </html>
